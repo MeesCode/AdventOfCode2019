48d957bf621c18a698e8b2a740c6062432368ad6
Mees <m.brinkhuis@student.tudelft.nl>
2019-12-10 23:52:05 +0100

    cleaned up day 10 and added animation

--- 10-2.py
+++ 10-2.py
@@ -1,83 +1,57 @@
-
+import os
 import math
+import time
 
-# get data
 data = [line.rstrip('\n') for line in open("data.txt")]
-
-# get a list of positions
-positions = []
+pos = []
 for y in range(len(data)):
 	for x in range(len(data[y])):
 		if data[y][x] == '#':
-			positions.append((x, y))
+			pos.append((x, y))
 
-max_spotted = 0
-best_position = (0,0)
-degrees_comet = []
-list_comet = []
-for origin in positions:
-# origin = (0,0)
-	spotted = []
-	degrees = []
-	ox, oy = origin
-	for comet in positions:
-		cx, cy = comet
-		if origin == comet:	# if the same; continue
-			continue
-		elif cy == oy: # if same y axis 
-			d = cx > ox and 90 or 270
-		elif cx == ox:  # if same x axis
-			d = cy > oy and 180 or 0
-		else: # calculate angle
-			if cx > ox and cy > oy:	# bottom right
-				d = 180-math.degrees(math.atan(abs(cx-ox)/abs(cy-oy)))
-			elif cx > ox and cy < oy:	# top right
-				d = math.degrees(math.atan(abs(cx-ox)/abs(cy-oy)))
-			elif cx < ox and cy < oy:	# top left
-				d = 360-math.degrees(math.atan(abs(cx-ox)/abs(cy-oy)))
-			else: # bottom left
-				d = 180+math.degrees(math.atan(abs(cx-ox)/abs(cy-oy)))
-		spotted.append(comet)
-		degrees.append(d)
-	if len(set(degrees)) > max_spotted:
-		list_comet = spotted
-		max_spotted = len(set(degrees))
-		degrees_comet = degrees
-		best_position = origin
+# part one
+asteroids = []
+ms = None
+size = 0
+for origin in pos:
+	comets = []
+	phi = []
+	dist = []
+	for comet in pos:
+		if origin == comet: continue
+		dx, dy = origin[0]-comet[0], origin[1]-comet[1]
+		d = math.atan2(dx, dy)
+		d = abs(d) if d <= 0 else 2*math.atan2(0, -1)-d
+		comets.append(comet)
+		phi.append(d)
+		dist.append(math.sqrt(dx*dx+dy*dy))
+	if len(set(phi)) > size:
+		size = len(set(phi))
+		asteroids = [list(i) for i in sorted(sorted(
+			zip(phi, comets, dist, [0]*len(phi)), 
+			key=lambda x: x[2]),
+			key=lambda x: x[0])]
+		ms = origin
 
-# print("best position", best_position)
-
-# we now know the best position, create
-# and ordered list of comets to be destroyed
-def getDegrees(item):
-	return item[0]
-
-def getDistance(item):
-	return item[2]
-
-distances = [math.sqrt(math.pow(best_position[0] - i[0], 2) + math.pow(best_position[1] - i[1], 2)) for i in list_comet]
-# print(distances)
-destroyed = [0] * len(list_comet)
-comet_list = sorted(sorted(zip(degrees_comet, list_comet, distances, destroyed), key=getDistance), key=getDegrees)
-comet_list = [list(i) for i in comet_list]
-
-destroyed_degrees = []
-destroyed_counter = 0
-index = 0
-last_destroyed = []
-while(destroyed_counter < 200):
-	if index >= len(comet_list):
-		index = 0
-		destroyed_degrees = []
-		# print("swoop")
-	c = comet_list[index]
-	if c[3] == 0 and c[0] not in destroyed_degrees:
+# part two
+i, cd, phi = 0, 0, -1
+last = []
+while(cd < 200):
+	if i >= len(asteroids): i = 0
+	c = asteroids[i]
+	if c[3] == 0 and c[0] != phi:
+		l = list(data[c[1][1]])
+		l[c[1][0]] = '.'
+		data[c[1][1]] = ''.join(l)
+		os.system('clear') 
+		[print(i) for i in data]
+		print()
+		time.sleep(0.020)
 		c[3] = 1
-		destroyed_degrees.append(c[0])
-		destroyed_counter += 1
-		last_destroyed = c
-		# print("destroyed:", destroyed_counter, c[1])
-	index += 1
+		phi = c[0]
+		cd += 1
+		last = c
+	i += 1
 
-print(last_destroyed[1][0]*100+last_destroyed[1][1])
-# [print(i) for i in comet_list]
+print("monitoring station", ms, "\nasteroid is sight", size)
+print("200th unlucky asteroid", last[1][0]*100+last[1][1])
