d1f76980a19fab3a6d4f9ffb6b8132130c7ade4f
Mees <m.brinkhuis@student.tudelft.nl>
2019-12-12 23:16:47 +0100

    3d animation day 12

--- 12-1.py
+++ 12-1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools
+import time
 
 class Moon:
     vx, vy, vz = 0, 0, 0
@@ -9,7 +10,8 @@ class Moon:
         self.z, self.oz = z, z
 
     def energy(self):
-        return sum([abs(i) for i in [self.x + self.y + self.z]]) * sum([abs(i) for i in [self.vx + self.vy + self.vz]])
+        return (sum([abs(i) for i in [self.x + self.y + self.z]]) * 
+            sum([abs(i) for i in [self.vx + self.vy + self.vz]]))
 
 m = [Moon(p[0], p[1], p[2]) for p in 
 [[int(i.split('=')[1]) for i in j] for j in 
@@ -17,7 +19,7 @@ m = [Moon(p[0], p[1], p[2]) for p in
 [line.rstrip('\n') for line in 
 open("data.txt")]]]]
 
-px, py, pz = 0,0,0
+px, py, pz = 0, 0, 0
 for c in itertools.count():
     for (x, y) in [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]:
         a, b = m[x], m[y]
@@ -29,14 +31,24 @@ for c in itertools.count():
         if a.z > b.z: a.vz, b.vz = a.vz-1, b.vz+1
         elif a.z < b.z: a.vz, b.vz = a.vz+1, b.vz-1
 
+    pic = [[' ']* 80 for i in range(80)]
+
     for i in m:
         i.x += i.vx
         i.y += i.vy
         i.z += i.vz
 
-    if px == 0 and m[0].x == m[0].ox and m[1].x == m[1].ox and m[2].x == m[2].ox and m[3].x == m[3].ox : px = c + 2
-    if py == 0 and m[0].y == m[0].oy and m[1].y == m[1].oy and m[2].y == m[2].oy and m[3].y == m[3].oy : py = c + 2
-    if pz == 0 and m[0].z == m[0].oz and m[1].z == m[1].oz and m[2].z == m[2].oz and m[3].z == m[3].oz : pz = c + 2
+        if i.z > 5: pic[(i.y//3)+40][(i.x//3)+40] = '⬤'
+        elif i.z < abs(5): pic[(i.y//3)+40][(i.x//3)+40] = '⚫'
+        else: pic[(i.y//3)+40][(i.x//3)+40] = '.'
+
+    [print(''.join(i)) for i in pic]
+    print()
+    time.sleep(0.1)
+
+    if px == 0 and not False in [i.x == i.ox for i in m]: px = c+2
+    if py == 0 and not False in [i.y == i.oy for i in m]: py = c+2
+    if pz == 0 and not False in [i.z == i.oz for i in m]: pz = c+2
     if 0 not in [px, py, pz]: break
 
 print("energy", sum([i.energy() for i in m])) # part 1
